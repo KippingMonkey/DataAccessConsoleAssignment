588232d3339ff8159ce5176d73bbb93a42208032
KippingMonkey <jonas@kippingmonkey.se>
2021-10-26 23:05:59 +0200

    add skeleton code. Install EnitityFWCore nugets

--- Program.cs
+++ Program.cs
@@ -1,12 +1,182 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Linq;
 
-namespace DataAccessConsoleAssignment
+namespace Assignment1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            bool running = true;
+            while (running)
+            {
+                int selected = Utils.ShowMenu("What do you want to do?", new[] {
+                    "List Movies",
+                    "Add Movie",
+                    "Delete Movie",
+                    "Load Movies from CSV File",
+                    "List Screenings",
+                    "Add Screening",
+                    "Delete Screening",
+                    "Exit"
+                });
+                Console.Clear();
+
+                if (selected == 0) ListMovies();
+                else if (selected == 1) AddMovie();
+                else if (selected == 2) DeleteMovie();
+                else if (selected == 3) LoadMoviesFromCSVFile();
+                else if (selected == 4) ListScreenings();
+                else if (selected == 5) AddScreening();
+                else if (selected == 6) DeleteScreening();
+                else running = false;
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void ListMovies()
+        {
+        }
+
+        public static void AddMovie()
+        {
+        }
+
+        public static void DeleteMovie()
+        {
+        }
+
+        public static void LoadMoviesFromCSVFile()
+        {
+        }
+
+        public static void ListScreenings()
+        {
+        }
+
+        public static void AddScreening()
+        {
+        }
+
+        public static void DeleteScreening()
+        {
+        }
+    }
+
+    public static class Utils
+    {
+        public static string ReadString(string prompt)
+        {
+            Console.Write(prompt + " ");
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        public static int ReadInt(string prompt)
+        {
+            Console.Write(prompt + " ");
+            int input = int.Parse(Console.ReadLine());
+            return input;
+        }
+
+        public static DateTime ReadDate(string prompt)
+        {
+            Console.WriteLine(prompt);
+            int year = ReadInt("Year:");
+            int month = ReadInt("Month:");
+            int day = ReadInt("Day:");
+            var date = new DateTime(year, month, day);
+            return date;
+        }
+
+        public static DateTime ReadFutureDate(string prompt)
+        {
+            var dates = new[]
+            {
+                DateTime.Now.Date,
+                DateTime.Now.AddDays(1).Date,
+                DateTime.Now.AddDays(2).Date,
+                DateTime.Now.AddDays(3).Date,
+                DateTime.Now.AddDays(4).Date,
+                DateTime.Now.AddDays(5).Date,
+                DateTime.Now.AddDays(6).Date,
+                DateTime.Now.AddDays(7).Date
+            };
+            var wordOptions = new[] { "Today", "Tomorrow" };
+            var nameOptions = dates.Skip(2).Select(d => d.DayOfWeek.ToString());
+            var options = wordOptions.Concat(nameOptions);
+            int daysAhead = ShowMenu(prompt, options.ToArray());
+            var selectedDate = dates[daysAhead];
+            return selectedDate;
+        }
+
+        public static void WriteHeading(string text)
+        {
+            Console.WriteLine(text);
+            string underline = new string('-', text.Length);
+            Console.WriteLine(underline);
+        }
+
+        public static int ShowMenu(string prompt, string[] options)
+        {
+            if (options == null || options.Length == 0)
+            {
+                throw new ArgumentException("Cannot show a menu for an empty array of options.");
+            }
+
+            Console.WriteLine(prompt);
+
+            int selected = 0;
+
+            // Hide the cursor that will blink after calling ReadKey.
+            Console.CursorVisible = false;
+
+            ConsoleKey? key = null;
+            while (key != ConsoleKey.Enter)
+            {
+                // If this is not the first iteration, move the cursor to the first line of the menu.
+                if (key != null)
+                {
+                    Console.CursorLeft = 0;
+                    Console.CursorTop = Console.CursorTop - options.Length;
+                }
+
+                // Print all the options, highlighting the selected one.
+                for (int i = 0; i < options.Length; i++)
+                {
+                    var option = options[i];
+                    if (i == selected)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine("- " + option);
+                    Console.ResetColor();
+                }
+
+                // Read another key and adjust the selected value before looping to repeat all of this.
+                key = Console.ReadKey().Key;
+                if (key == ConsoleKey.DownArrow)
+                {
+                    selected = Math.Min(selected + 1, options.Length - 1);
+                }
+                else if (key == ConsoleKey.UpArrow)
+                {
+                    selected = Math.Max(selected - 1, 0);
+                }
+            }
+
+            // Reset the cursor and return the selected option.
+            Console.CursorVisible = true;
+            return selected;
         }
     }
 }
